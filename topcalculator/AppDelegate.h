4927f0b69d2806a4cde42113ae6b7b172d56a9cf
Alice Tsai <chiafang@thinkpresent-lm.corp.yahoo.com>
2014-02-28 17:31:46 -0800

    Initial Commit

---
+++ topcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  topcalculator
+//
+//  Created by Alice Tsai on 2/28/14.
+//  Copyright (c) 2014 Alice Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ topcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  topcalculator
+//
+//  Created by Alice Tsai on 2/28/14.
+//  Copyright (c) 2014 Alice Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
